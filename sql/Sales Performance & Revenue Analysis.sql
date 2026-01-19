f6156986fdc5307f9558748fea88dd6def7e84d5
teenuawachat <154955067+teenuawachat@users.noreply.github.com>
2026-01-19 14:50:27 +0530

    Update Sales Performance & Revenue Analysis.sql

--- sql/Sales Performance & Revenue Analysis.sql
+++ sql/Sales Performance & Revenue Analysis.sql
@@ -145,7 +145,24 @@ WHERE o.status = "Completed"
 GROUP BY p.category
 ORDER BY `Average Discount`,`Total Revenue` DESC;
 
-
+-- FACT TABLE FOR POWERBI
+SELECT 
+    o.order_id,
+    o.order_date,
+    c.customer_id,
+    c.customer_name,
+    c.region,
+    p.product_name,
+    p.category,
+    oi.quantity,
+    p.price,
+    oi.discount,
+    (oi.quantity * p.price * (1 - oi.discount)) AS revenue
+FROM orders o
+JOIN customers c ON o.customer_id = c.customer_id
+JOIN order_items oi ON o.order_id = oi.order_id
+JOIN products p ON oi.product_id = p.product_id
+WHERE o.status = 'Completed';
 
 
 
